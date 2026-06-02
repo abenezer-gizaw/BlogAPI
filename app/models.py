6681b27fb1362a5b14b5445d54578563a0d37fd5
Abenezer <kidaneabena@gmail.com>
2026-06-02 16:58:18 -0400

    Like Crud

--- app/models.py
+++ app/models.py
@@ -34,6 +34,9 @@ class Post(Base):
 
     owner = relationship("User", back_populates="posts")
     comments = relationship("Comment", back_populates="post", cascade="all, delete")
+    #with out cascade SQLAlchemy may complain because those comments still reference the deleted post through post_id.
+    #You could get errors such as foreign key constraint violations. so cascade make sure all the commnets get deleted
+    #when it is
 
 class Comment(Base):
     __tablename__ = "comments"
@@ -46,3 +49,14 @@ class Comment(Base):
 
     user = relationship("User")
     post = relationship("Post", back_populates="comments")
+
+
+#the combination of (user_id, post_id) must be unique per row for likes
+#A post can be liked by many users
+#But a user cannot like the same post twice
+class Like(Base):
+    __tablename__ = "likes"
+
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
+
+    post_id = Column(Integer,ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True)
---
+++ app/routers/likes.py
@@ -0,0 +1,46 @@
+#The goal is:
+#user can like a post.
+#user can unlike a post.
+#user cannot like the same post twice.
+#Each post can have many likes.
+#Each user can like many posts.
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.orm import Session
+from ..database import get_db
+from.auth import get_current_user
+from ..models import Like, Post
+from typing import Annotated
+
+router = APIRouter(
+    prefix='/posts',
+    tags=["likes"]
+)
+
+db_dependency = Annotated [Session, Depends(get_db)]
+jwt_depencency = Annotated[dict, Depends(get_current_user)]
+@router.post("/{post_id}/likes")
+def like_post( post_id:int, db: db_dependency, user:jwt_depencency):
+    if user is None:
+        raise HTTPException( status_code=401, detail="Not authenticated")
+    post = db.query(Post).filter( Post.id == post_id).first()
+    if not post:
+        raise HTTPException(status_code=404,detail="Post not found")
+    
+    existing_like = db.query(Like).filter(Like.user_id == user.get('id'), Like.post_id == post_id).first()
+    if existing_like:
+        raise HTTPException(status_code=409, detail="Already liked")
+    new_like = Like(user_id=user.get("id"), post_id=post_id)
+
+    db.add(new_like)
+    db.commit()
+    return { "message": "Post liked successfully"} # i might need to add the name of the liked post
+@router.delete("/{post_id}/unlike")
+def unlike_post(post_id: int, db: db_dependency, user:jwt_depencency):
+    if user is None:
+        raise HTTPException( status_code=401, detail="Not authenticated")
+    like = db.query(Like).filter( Like.user_id == user.get("id"), Like.post_id == post_id).first()
+    if not like:
+        raise HTTPException (status_code=404, detail="Like not found")
+    db.delete(like)
+    db.commit()
+    return {"message": "Like removed"}
